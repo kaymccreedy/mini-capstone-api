ac9c116c4dc4cd688745825787256c2b5fc7e79d
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-06-30 12:53:45 -0400

    adds products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,19 @@
 class ProductsController < ApplicationController
+
+  def products
+    products = Product.all.order(:id)
+    productlist = []
+    products.each do |product|
+      info = "Item: #{product[:name]} | Price: #{product[:price]} | Description: #{product[:description]}"
+      productlist << info
+    end
+    render json: {"Products:": productlist}
+  end
+
+  def contact1
+    contact = Contact.first
+    info = "First name: #{contact[:first_name]} | Last name: #{contact[:last_name]} | Email: #{contact[:email]} | Phone number: #{contact[:phone_number]}"
+    render json: {"Contact Info:": info}
+  end
+
 end
