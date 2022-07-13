3b3589cf0470a3032064d43ca884b9cd5e860fb2
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-07-13 15:26:49 -0400

    updates authentications

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
   post "/users" => "users#create"
 
+  patch "/users/:id" => "users#update"
+
   post "/sessions" => "sessions#create"
 
   get "/orders" => "orders#index"
