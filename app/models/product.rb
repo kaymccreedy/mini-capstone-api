c9edd3d96365dc71698e658f4816157d9af3a551
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-07-15 14:16:28 -0400

    updates mini capstone

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_many :orders
   belongs_to :supplier
+  has_many :category_products
+  has_many :categories, through: :category_products
   # validates :name, presence: true, uniqueness: true
   # validates :price, presence: true, numericality: { greater_than: 0 }
   # validates :description, length: { in: 10..500 }
---
+++ repeat.rb
@@ -0,0 +1,5 @@
+x = 6
+15.times do
+  puts "CategoryProduct.create(product_id: #{x}, category_id: 3)"
+  x += 1
+end
